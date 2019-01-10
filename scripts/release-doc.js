678221226c4faf1e338e8c01709176a25f56cf59
bloss <stickmyc@163.com>
2019-01-10 17:36:59 +0800

    build doc scripts

---
+++ scripts/release-doc.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const fs = require('fs-extra');
+const chalk = require('chalk');
+const spawn = require('child_process').spawn;
+
+const buildPath = path.resolve(process.cwd(), 'docs');
+const docsPath = path.resolve(process.cwd(), 'documentation');
+
+function runCommand(cmd, args, options) {
+	return new Promise((resolve, reject) => {
+		const spwan = spawn(
+			cmd,
+			args,
+			Object.assign(
+				{
+					cwd: process.cwd(),
+					stdio: 'inherit',
+					shell: true,
+				},
+				options,
+			),
+		);
+
+		spwan.on('exit', () => {
+			resolve();
+		});
+	});
+}
+
+function clean() {
+	fs.removeSync(buildPath);
+}
+
+function copy() {
+	fs.ensureDirSync(buildPath);
+	fs.copySync(path.resolve(docsPath, 'build'), buildPath);
+}
+
+function start() {
+	runCommand('docanary', ['build'], {
+		cwd: docsPath,
+	}).then(() => {
+		clean();
+		copy();
+	}).catch(e => {
+		console.log(chalk.red(e));
+	});
+}
+
+start();
