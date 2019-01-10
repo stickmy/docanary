75d4ebf2db942d583f4b1542f273f5fa3cbcbe7a
bloss <stickmyc@163.com>
2019-01-10 19:04:16 +0800

    remove documentation folder

--- documentation/components/AlphaTag/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './index.styl?module';
-
-function AlphaTag(props) {
-	return <span className={styles.alphaTag}>{props.children}</span>;
-}
-
-export default AlphaTag;
--- documentation/components/TodoTag/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './index.styl?module';
-
-function TodoTag(props) {
-	return <span className={styles.todoTag}>{props.children}</span>;
-}
-
-export default TodoTag;
